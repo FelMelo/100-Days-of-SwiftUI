91554134863479c9ab2342ab154ce99c88197577
Felipe <felipecmelo95@icloud.com>
2020-03-31 11:15:55 -0300

    First commit, day1 completed

---
+++ TheCourse/Day1/Day1-Playground.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+//Variables
+var str1 = "Hello, playground"
+str1 = "Goodbye"
+
+//Strings and Integers
+var str2 = "Hello, playground"
+var age = 24
+var population = 8_000_000
+
+//Multi-Line Strings
+var str3 = """
+This string
+goes over
+multiple lines.
+"""
+var str4 = """
+This string \
+goes over \
+multiple lines.
+"""
+
+//Doubles and Booleans
+var double = 3.141516
+var boolean = true
+
+//String Interpolation
+var score = 57
+var str5 = "Your score was \(score)"
+var result = "The test results are here: \(str5)"
+
+//Constants
+let yearOfBirth = 1995
+
+//Type Annotations
+let str6 = "Hello, playground"
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+
